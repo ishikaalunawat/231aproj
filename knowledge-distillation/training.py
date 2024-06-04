5ee474409e4922aaf5da50d54f400995ed7c04af
Aditya Dutt <asdutt2@stanford.edu>
2024-06-03 18:02:15 -0700

    Modified training.py

--- knowledge-distillation/training.py
+++ knowledge-distillation/training.py
@@ -48,7 +48,7 @@ Arguments:
 
     args - Other required arguments: [epochs, lr, device, weight_decay, dry_run]
 """
-def train(model, train_dataloader, val_dataloader, args):
+def train(model, train_dataloader, args):
     
     #Parse the args
     epochs = args.epochs
@@ -61,94 +61,55 @@ def train(model, train_dataloader, val_dataloader, args):
     optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=args.weight_decay)
     criterion = nn.MSELoss()
 
-    # Define Train Function for each epoch
-    def train_fn(current_epoch):
+    # Training loop
+    for epoch in range(args.epochs):
         model.train()
         total_loss = 0.0
-        tk = tqdm(train_dataloader, desc="EPOCH" + "[TRAIN]" + str(current_epoch + 1) + "/" + str(epochs))
-
-        for t, data in enumerate(tk):
-            images, labels = data
-            images, labels = images.to(device), labels.to(device)
+        for inputs, labels in train_dataloader:
+            inputs = inputs.to(device)
             optimizer.zero_grad()
-            logits = model(images)
-            loss = criterion(logits, labels)
+            outputs = model(inputs)
+            # Placeholder for 3D points labels; replace with actual labels
+            loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-
             total_loss += loss.item()
-            tk.set_postfix({"Loss": "%6f" % float(total_loss / (t + 1))})
-            if dry_run:
-                break
+        print(f'Epoch {epoch + 1}, Loss: {total_loss / len(train_dataloader)}')
 
-        return total_loss / len(train_dataloader)
 
-    # Define Validation Function for each epoch
-    def eval_fn(current_epoch):
-        model.eval()
-        total_loss = 0.0
-        tk = tqdm(val_dataloader, desc="EPOCH" + "[VALID]" + str(current_epoch + 1) + "/" + str(epochs))
+    ## Saving model
+    torch.save(model.state_dict(), "student_models/scene_{}.pth".format(args.scene_type))
 
-        for t, data in enumerate(tk):
-            images, labels = data
-            images, labels = images.to(device), labels.to(device)
+def test(model, test_dataloader, args):
+    model.eval()
+    criterion = nn.MSELoss()
 
-            logits = model(images)
-            loss = criterion(logits, labels)
-
+    total_loss = 0.0
+    with torch.no_grad():
+        for inputs, labels in test_dataloader:
+            inputs = inputs.to(args.device)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
             total_loss += loss.item()
-            tk.set_postfix({"Loss": "%6f" % float(total_loss / (t + 1))})
-            if dry_run:
-                break
+    print(f'Test Loss: {total_loss / len(test_dataloader)}')
 
-        return total_loss / len(val_dataloader)
 
-    best_valid_loss = np.inf
-    best_train_loss = np.inf
-    for i in range(epochs):
-        train_loss = train_fn(i)
-        val_loss = eval_fn(i)
-
-        # Can Modify this
-        if val_loss < best_valid_loss:
-            torch.save(model.state_dict(), "best-weights.pt")
-            print("Saved Best Weights")
-            best_valid_loss = val_loss
-            best_train_loss = train_loss
-
-    print(f"Training Loss : {best_train_loss}")
-    print(f"Valid Loss : {best_valid_loss}")
 
 def get_args_parser():
     parser = argparse.ArgumentParser(description='Student Model Training Args')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--patch-size', type=int, default=16,
-                        help='patch size for images (default : 16)')
-    parser.add_argument('--latent-size', type=int, default=768,
-                        help='latent size (default : 768)')
-    parser.add_argument('--n-channels', type=int, default=3,
-                        help='number of channels in images (default : 3 for RGB)')
-    parser.add_argument('--num-heads', type=int, default=12,
-                        help='(default : 12)')
-    parser.add_argument('--num-encoders', type=int, default=12,
-                        help='number of encoders (default : 12)')
-    parser.add_argument('--dropout', type=int, default=0.1,
-                        help='dropout value (default : 0.1)')
-    parser.add_argument('--img-size', type=int, default=224,
-                        help='image size to be reshaped to (default : 224')
-    parser.add_argument('--num-classes', type=int, default=10,
-                        help='number of classes in dataset (default : 10 for CIFAR10)')
-    parser.add_argument('--epochs', type=int, default=10,
-                        help='number of epochs (default : 10)')
-    parser.add_argument('--lr', type=float, default=1e-2,
-                        help='base learning rate (default : 0.01)')
-    parser.add_argument('--weight-decay', type=int, default=3e-2,
-                        help='weight decay value (default : 0.03)')
-    parser.add_argument('--batch-size', type=int, default=4,
-                        help='batch size (default : 4)')
-    parser.add_argument('--dry-run', action='store_true', default=False,
-                        help='quickly check a single pass')
+    parser.add_argument('--no-cuda', action='store_true', default=False, help='disables CUDA training')
+    parser.add_argument('--patch-size', type=int, default=16, help='patch size for images (default : 16)')
+    parser.add_argument('--latent-size', type=int, default=256, help='latent size (default : 256)')
+    parser.add_argument('--n-channels', type=int, default=3, help='number of channels in images (default : 3 for RGB)')
+    parser.add_argument('--num-heads', type=int, default=12, help='(default : 12)')
+    parser.add_argument('--num-encoders', type=int, default=12, help='number of encoders (default : 12)')
+    parser.add_argument('--dropout', type=float, default=0.1, help='dropout value (default : 0.1)')
+    parser.add_argument('--img-size', type=int, default=224, help='image size to be reshaped to (default : 224)')
+    parser.add_argument('--batch-size', type=int, default=4, help='batch size (default : 4)')
+    parser.add_argument('--epochs', type=int, default=10, help='number of epochs (default : 10)')
+    parser.add_argument('--lr', type=float, default=1e-2, help='base learning rate (default : 0.01)')
+    parser.add_argument('--weight-decay', type=float, default=3e-2, help='weight decay value (default : 0.03)')
+    parser.add_argument('--dry-run', action='store_true', default=False, help='quickly check a single pass')
     
     ### Previous args ###
     parser.add_argument("--weights_path", type=str, default='cuda', help="pytorch device")
@@ -240,14 +201,21 @@ def main():
     create_dataset_labels(pts3D, args)
 
     ## create dataset using 3D points predicted by above teacher model
-    train_dataloader = get_dataloader("datasets/12scenes_apt1_kitchen/train/", num_samples = args.num_train, batch_size=4)
-    test_dataloader = get_dataloader("datasets/12scenes_apt1_kitchen/test/", batch_size=1)
+    small_dataset_path = "datasets/12scenes_apt1_kitchen/"
+
+    train_dataloader = get_dataloader(small_dataset_path + "train/", num_samples = args.num_train, batch_size=4)
+    test_dataloader = get_dataloader(small_dataset_path + "test/", batch_size=1)
     # Start training
     student = StudentModel(args).to(args.device)
 
     ### Call train function on student model ###
     ### Need Train and Val DataLoaders ###
-    train(model = student, train_dataloader=train_dataloader, test_dataloader=test_dataloader, args = args)
+    train(model = student, train_dataloader=train_dataloader, args = args)
+
+    test(model=student, test_dataloader=test_dataloader, args=args)
+
+
+
 
 if __name__ == "__main__":
     main()
