79ae57f65c13bb8b29dd4f5cd7ca367c864a5ff2
ishikaalunawat <ishikaa@stanford.edu>
2024-06-10 08:02:31 +0000

    added

--- knowledge-distillation/dataloader.py
+++ knowledge-distillation/dataloader.py
@@ -1,26 +1,38 @@
+# import sys
+# sys.path.append("dust3r/")
+
 import os
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from PIL import Image
 import numpy as np
+import sys, os
 
 from dust3r.utils.image import load_images, rgb
 
 class RGBDDataset(Dataset):
-    def __init__(self, scene_dir, transform=False):
-        self.scene_dir = scene_dir
+    def __init__(self, root_dir, scene_type, split="train", train_mode=True, transform=False):
+        self.scene_dir = os.path.join(root_dir, scene_type, split)
+        self.train = train_mode
         self.frames = self._load_frames()
 
     def _load_frames(self):
         frames = []
-        if os.path.isdir(self.scene_dir):
-            for f in os.listdir(os.path.join(self.scene_dir, "rgb")):
-                if f.endswith(".color.jpg"):
-                    frame_id = f.split(".")[0]
+        if not os.path.isdir(self.scene_dir):
+            self.scene_dir = os.path.join(os.getcwd(), self.scene_dir)
+
+        for f in os.listdir(os.path.join(self.scene_dir, "rgb")):
+            if f.endswith(".color.jpg"):
+                frame_id = f.split(".")[0]
+                if self.train:
                     frames.append({
                         'color': os.path.join(self.scene_dir, "rgb", f"{frame_id}.color.jpg"),
                         'pts3d': os.path.join(self.scene_dir, "pts3d", f"{frame_id}.pt"),
+                    })
+                else:
+                    frames.append({
+                        'color': os.path.join(self.scene_dir, "rgb", f"{frame_id}.color.jpg")
                     })
 
         return frames
@@ -30,16 +42,19 @@ class RGBDDataset(Dataset):
 
     def __getitem__(self, idx):
         frame_info = self.frames[idx]
+        scene_coordinate_image = None
 
         # load RGB image
-        rgb_image = load_images([frame_info['color']], size=512)[0]['img'].squeeze().to(device="cuda")
+        rgb_image = load_images([frame_info['color']], size=512, verbose=False)[0]['img'].squeeze()
 
         # load scene coordinate image
-        scene_coordinate_image = torch.load(frame_info['pts3d']).to(device="cuda")
+        if self.train:
+            scene_coordinate_image = torch.load(frame_info['pts3d'])
+            return rgb_image, scene_coordinate_image
+        else:
+            return rgb_image
 
-        return rgb_image, scene_coordinate_image
-
-def get_dataloader(root_dir, batch_size=32, transform=False, shuffle=True):
-    dataset = RGBDDataset(root_dir, transform=True)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+def get_dataloader(root_dir, scene_type, split, train_mode=True, batch_size=16, transform=False, shuffle=True):
+    dataset = RGBDDataset(root_dir, scene_type, split, train_mode=train_mode, transform=True)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=0)
     return dataloader
